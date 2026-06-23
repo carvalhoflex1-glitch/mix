d8f11c534504e3ea6866696b1925df949576884d
carvalhoflex1-glitch <carvalhoflex1@gmail.com>
2026-06-23 23:41:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -188,8 +188,8 @@ def coin_fmt(value, asset):
     value = D(value)
     precision = Decimal("0.000001") if asset == "LTC" else Decimal("0.01")
     out = value.quantize(precision, rounding=ROUND_DOWN)
-    if asset == "TL":
-        return f"{{{{TL}}}}{out}"
+if asset == "TL":
+    return f"{out} {{{{TL}}}}"
     return f"{out} {{{{{asset}}}}}"
 
 
